b322df68ad2b4170a77248d79152309875d83081
IAmHereHello <michaelvandera@gmail.com>
2026-01-15 21:22:45 +0100

    Added speed stats.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "./contexts/LanguageContext";
@@ -30,6 +31,7 @@ const ProtectedRoute = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
+      <SpeedInsights />
       <LanguageProvider>
         <TooltipProvider>
           <Toaster />
